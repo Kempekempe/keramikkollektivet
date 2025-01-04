ee81d0d692709ad639efc24c351e99f311caeced
Linnea Kempe <linnea.kempe1@ingka.ikea.com>
2025-01-04 19:46:17 +0100

    chore: update project structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/App.scss';
+
+
+const Home = () => {
+  return (
+    <>
+      <div className='hero'>
+        <img src='/images/logo.svg' alt="Logo" className="hero__logo"></img>
+        <div className='hero__image'></div>
+      </div>
+{/*       <div>Tis is the next section</div>
+ */}    </>
+  );
+};
+
+export default Home;
