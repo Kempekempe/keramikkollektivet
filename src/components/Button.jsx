3d55cd3f2b4352821be61f5d4a87ce7ed12db483
Linnea Kempe <linnea.kempe1@ingka.ikea.com>
2025-01-04 21:45:42 +0100

    Deploy production build to GitHub Pages

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import '../styles/_button.scss'
+
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button>HEJ</button>
+  );
+};
+
+export default Button;
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../components/Button.jsx'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about home page.</p>
+      <Button/>
+    </div>
+  );
+};
+
+export default About;
